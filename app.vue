85944eca2447555f86edf9544674fd919820dd10
Abdurahmon086 <1937ali047@gmail.com>
2023-11-26 18:12:07 +0500

    first commit

---
+++ app.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="absolute top-0 left-0 z-50  w-full">
+    <div
+      class="container mx-auto h-16 flex items-center justify-between border-b-[1px] border-white"
+    >
+      <div class="flex items-center gap-3">
+        <img
+          class="w-5 h-6"
+          src="https://static.tildacdn.com/tild3464-3432-4365-b439-383037373638/logo_goodwill.svg"
+          alt="logo icon"
+        />
+        <h4 class="text-[14px]">Goodwill Realty</h4>
+      </div>
+      <nav>
+        <ul class="flex items-center gap-10">
+          <li class=""><a href="#" class="text-[14px]"> О компании </a></li>
+          <li class=""><a href="#" class="text-[14px]"> Проекты </a></li>
+          <li class=""><a href="#" class="text-[14px]"> Преимущества покупки </a></li>
+          <li class=""><a href="#" class="text-[14px]"> Контакты </a></li>
+        </ul>
+      </nav>
+      <a href="tel:" class="text-[14px] mr-24">+ 971 5 886 95492</a>
+    </div>
+  </header>
+  <main>
+    <NuxtPage />
+  </main>
+  <footer><h4>FOOTER</h4></footer>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section
+    class="hero bg-[url(heroBg.jpg)] bg-cover bg-no-repeat h-[800px] pt-[200px]"
+  >
+    <div class="hero__wrapper container mx-auto flex">
+      <div class="hero__left">
+        <h1 class="text-[40px] max-w-[585px] font-semibold uppercase">
+          Подберём недвижимость в Дубае для жизни и инвестиций
+        </h1>
+        <em class="block text-[#f0eeee] text-[28px] uppercase font-semibold mt-5"
+          >С ПЕРВЫМ ВЗНОСОМ ОТ 15 000$</em
+        >
+      </div>
+      <div class="hero__right"></div>
+    </div>
+  </section>
+</template>
