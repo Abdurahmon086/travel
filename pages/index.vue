2e9d4f42dd78739a54585dc4e5c6e7a0f4fd86a3
XushnayevAsadbek <xushnaevasadbek@gmial.com>
2023-11-29 23:02:10 +0500

    sections commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,204 @@
-<template>
-  <section
-    class="hero bg-[url(heroBg.jpg)] bg-cover bg-no-repeat h-[800px] pt-[200px]"
-  >
-    <div class="hero__wrapper container mx-auto flex">
-      <div class="hero__left">
-        <h1 class="text-[40px] max-w-[585px] font-semibold uppercase">
-          Подберём недвижимость в Дубае для жизни и инвестиций
-        </h1>
-        <em class="block text-[#f0eeee] text-[28px] uppercase font-semibold mt-5"
-          >С ПЕРВЫМ ВЗНОСОМ ОТ 15 000$</em
-        >
+    <template>
+      <section
+        class="hero bg-[url(heroBg.jpg)] bg-cover bg-no-repeat h-[800px] pt-[200px]"
+      >
+        <div class="container1">
+          <div class="hero__left">
+            <h1 class="text-[40px] max-w-[585px] font-semibold uppercase">
+              Подберём недвижимость в Дубае для жизни и инвестиций
+            </h1>
+            <em class="block text-[#f0eeee] text-[28px] uppercase font-semibold mt-5"
+              >С ПЕРВЫМ ВЗНОСОМ ОТ 15 000$</em
+            >
+          </div>
+          <div class="hero__right"></div>
+        </div>
+      </section>
+      <section class="bg-[#F0EEEE] pt-[80px]">
+      <div class="container1">
+     <div class="flex flex-col items-center">
+      <button class="text-white text-base font-sans font-medium bg-[#403f3f] rounded-md outline-none cursor-pointer w-[250px] h-[50px] hover:bg-[#cca969] ">
+        Подобрать квартиру онлайн
+      </button>
+     </div>
+     <div class="flex justify-between mt-52">
+     <div>
+     <h2 class="text-4xl font-sans text-[#403f3f] font-medium w-[561px] leading-[54px]">ВОСПОЛЬЗУЙТЕСЬ НАШИМ БЕСПЛАТНЫМ СЕРВИСОМ ПО ПОДБОРУ НЕДВИЖИМОСТИ:</h2>
+     <ul class="mt-5 flex flex-col gap-y-6 ">
+     <li class="flex gap-x-11 items-center">
+     <img class="w-6 h-6" src="https://static.tildacdn.com/tild3561-3365-4234-b234-303535353663/Group_19.svg" alt="icon">
+     <p class="font-medium text-[#403f3f] text-lg font-sans ">
+      Все районы <span class="font-normal">города</span>
+     </p>
+     </li>
+     <li class="flex gap-x-11 items-center">
+     <img class="w-6 h-6" src="https://static.tildacdn.com/tild3561-3365-4234-b234-303535353663/Group_19.svg" alt="icon">
+     <p class="font-normal text-[#403f3f] ">
+      Подберем на ваш  <span class=" font-medium text-[#403f3f] text-lg font-sans">запрос и бюджет</span>
+     </p>
+     </li>
+     <li class="flex gap-x-11 items-center">
+     <img class="w-6 h-6" src="https://static.tildacdn.com/tild3561-3365-4234-b234-303535353663/Group_19.svg" alt="icon">
+     <p class="font-medium text-[#403f3f] text-lg font-sans ">
+      Более 100 000 вариантов  <span class="font-normal">от застройщиков</span>
+     </p>
+     </li>
+     <li class="flex gap-x-11 items-center">
+     <img class="w-6 h-6" src="https://static.tildacdn.com/tild3561-3365-4234-b234-303535353663/Group_19.svg" alt="icon">
+     <p class="font-normal text-[#403f3f] ">
+      Есть предложения с выгодными <br>  <span class=" font-medium text-[#403f3f] text-lg font-sans">акциями и скидками до 25%</span>
+     </p>
+     </li>
+     </ul>
+     </div>
+     <div class="relative">
+     <img class="w-[460px] h-[460px] relative" src="https://thumb.tildacdn.com/tild3334-3430-4461-b232-616531616536/-/resize/920x/-/format/webp/smartmockups_l9ctrhs.png" alt="">
+     <img class="w-[220px] h-[220px] absolute top-[280px] left-[300px]" src="https://thumb.tildacdn.com/tild3163-3038-4235-b639-386638616335/-/resize/440x/-/format/webp/Frame_2.png" alt="">
+     </div>
+     </div>
+     <button class="text-white text-base font-sans font-medium bg-[#cca969] rounded-md outline-none cursor-pointer w-[250px] h-[50px] hover:bg-[#403f3f] ">
+        Подобрать квартиру онлайн
+      </button>
       </div>
-      <div class="hero__right"></div>
-    </div>
-  </section>
-</template>
+      </section>
+      <section class="bg-[#F0EEEE] pt-[165px]">
+      <div class="container1">
+        <div class="flex flex-col  items-center ">
+        <h2 class="text-[#403f3f] text-4xl font-medium font-sans w-[506px]  leading-[54px] text-center mb-4">
+          Лидеры Goodwill Realty
+        </h2>
+        <p class="text-[#161616] text-[18px] font-normal w-[487px] font-sans leading-[28px] mb-16 text-center">Команда Goodwill Realty - это объединение профессионалов рынка недвижимости с любовью относящиеся к своему делу и каждому из Вас.</p>
+        </div>
+        <div class="flex justify-between gap-x-9">
+        <div class="group/item  relative transition-all card w-[360px] h-[400px]">
+        <img src="../public/pro.jpg" alt="Виталий Богданов-img">
+       <div class="group/edit invisible group-hover/item:visible absolute top-0 left-0 bg-[#00000044] w-full h-full flex flex-col justify-end gap-1 p-6" >
+        <h4 class="text-[#f9f7f7] text-base font-sans font-normal ">Виталий Богданов</h4>
+        <p class="text-[#dadada] text-xs font-normal font-sans w-[221px] mb-5">Президент компании Goodwill Realty. Профессиональный инвестор.</p>
+       </div>
+        </div>
+        <div class="group/item  relative transition-all card w-[360px] h-[400px]">
+        <img src="../public/pro1.jpg" alt="Оксана Бакаева-img">
+        <div class="group/edit invisible group-hover/item:visible absolute top-0 left-0 bg-[#00000044] w-full h-full flex flex-col justify-end gap-1 p-6" >
+        <h4 class="text-[#f9f7f7] text-base font-sans font-normal ">Оксана Бакаева</h4>
+        <p class="text-[#dadada] text-xs font-normal font-sans w-[202px] mb-5 ">Соучередитель и генеральный директор компании Goodwill Realty. Эксперт по недвижимости.</p>
+       </div>
+        </div>
+        <div class="group/item  relative transition-all card w-[360px] h-[400px]">
+          <img  src="../public/pro2.jpg" alt="Алексей Ершов-img">
+          <div class="group/edit invisible group-hover/item:visible absolute top-0 left-0 bg-[#00000044] w-full h-full flex flex-col justify-end gap-1 p-6">
+            <h4 class="text-[#f9f7f7] text-base font-sans font-normal ">Алексей Ершов</h4>
+            <p class="text-[#dadada] text-xs font-normal font-sans w-[221px]  mb-5">Соучередитель компании Goodwill Realty. Резидент бизнес-клуба Эквиум. Президент X10 Academy. Основал более 10 успешных компаний в России.</p>
+          </div>
+        </div>
+        </div>
+      </div>
+      </section>
+      <section class="bg-[#F0EEEE] pt-[140px]">
+        <div class="container1">
+        <div class="flex flex-col  items-center ">
+        <h2 class="text-[#403f3f] text-4xl font-medium font-sans w-[506px]  leading-[54px] text-center mb-4">
+          ОСТАВАЙТЕСЬ НА СВЯЗИ!
+        </h2>
+        <p class="text-[#161616] text-[18px] font-normal w-[593px] font-sans leading-[28px] mb-16">Подпишитесь на нас в социальных сетях и узнавайте об актуальных квартирах и условиях покупки первыми! А так же, каждую неделю мы делимся полезной информацией о рынке недвижимости в Дубае.</p>
+        </div>
+        <div class="flex gap-x-32 items-center border-b-[1px] border-[#a09d9d] pb-4  pt-5">
+        <div>
+        <img class="w-[180px] h-[180px] cursor-pointer" src="https://thumb.tildacdn.com/tild3166-6166-4832-b232-643434383364/-/cover/360x360/center/center/-/format/webp/Instagram_post_-_70.png" alt="goodwill_realty_dubai-img">
+        </div>
+        <div>
+        <div class="flex gap-x-3">
+          <img src="https://static.tildacdn.com/tild3837-3934-4131-b637-323333646430/instagram-svgrepo-co.svg" alt="goodwill_realty_dubai-icon">
+          <a class="text-[#161616] text-[18px] font-sans font-semibold leading-[28px]" href="#">goodwill_realty_dubai</a>
+          </div>
+          <p class="text-[#161616] text-sm font-medium my-5 font-sans">НЕДВИЖИМОСТЬ ДУБАЙ | ИНВЕСТИЦИИ | REAL ESTATE DUBAI</p>
+          <p class="text-[#161616] text-sm font-sans font-normal mb-2">- Квартиры на любой бюджет под любой запрос <br>
+            - Беспроцентная рассрочка, резиденство для всей семьи
+          </p>
+        </div>
+        </div>
+        <div class="flex gap-x-32 items-center border-b-[1px] border-[#a09d9d]  pb-4  pt-5">
+        <div>
+        <img class="w-[180px] h-[180px] cursor-pointer" src="https://thumb.tildacdn.com/tild6365-3235-4939-b762-373836393166/-/cover/360x360/center/center/-/format/webp/Instagram_post_-_18.png" alt="goodwillestate-img">
+        </div>
+        <div>
+        <div class="flex gap-x-3">
+          <img src="https://static.tildacdn.com/tild6535-3235-4463-b730-306136323966/Vector.svg" alt="goodwillestate-icon">
+          <a class="text-[#161616] text-[18px] font-sans font-semibold leading-[28px]" href="#">@goodwillestate</a>
+          </div>
+          <p class="text-[#161616] text-sm font-medium my-5 font-sans">Жизнь в Дубае | Недвижимость | Инвестиции | Goodwill Realty</p>
+          <p class="text-[#161616] text-sm font-sans font-normal mb-2">
+                Блог о недвижимости, инвестициях и жизни в Дубае <br>
+                - Обзоры квартир, таунхаусов, вилл <br>
+                - Редкие проекты, которые прямо сейчас можно приобрести по лучшей цене до официального старта продаж <br>
+                - Новости, аналитика и рекомендации
+    </p>
+
+        </div>
+        </div>
+        <div class="flex gap-x-32 items-center border-b-[1px] border-[#a09d9d]  pb-4  pt-5">
+        <div>
+        <img class="w-[180px] h-[180px] cursor-pointer" src="https://thumb.tildacdn.com/tild3534-3930-4234-b836-336435643334/-/cover/360x360/center/center/-/format/webp/Group_125.png" alt="goodwillrealty-img">
+        </div>
+        <div>
+        <div class="flex gap-x-3">
+          <img src="https://static.tildacdn.com/tild3462-6331-4039-b831-383038353662/Group.svg" alt="goodwillrealty-icon">
+          <a class="text-[#161616] text-[18px] font-sans font-semibold leading-[28px]" href="#">@goodwillrealty</a>
+          </div>
+          <p class="text-[#161616] text-sm font-medium my-5 font-sans">Недвижимость в Дубае | Инвестиции | GOODWILL</p>
+          <p class="text-[#161616] text-sm font-sans font-normal mb-2">На канале рассказываем:<br>
+- Как правильно выбрать недвижимость в зависимости от цели — проживание, сдача в аренду, последующая перепродажа итд; <br>
+- Как устроен рынок недвижимости в Дубае и во что выгодно вкладываться сегодня; <br>
+- Что вообще нужно знать иностранцу о жизни в Дубае, чтобы сделать ее комфортной.</p>
+
+        </div>
+        </div>
+        </div>
+      </section>
+      <section class="bg-[#F0EEEE] pt-[165px]">
+      <div class="container1">
+      <h2 class="text-[#403f3f] text-4xl font-medium font-sans w-[506px]  leading-[54px] pb-12">НАШИ ОФИСЫ В МОСКВЕ, САНКТ-ПЕТЕБУРГЕ И ДУБАЕ</h2>
+      <div class="grid grid-cols-2 gap-x-5">
+      <div class="mb-6">
+      <img class="w-[570px] h-[427.5px] cursor-zoom-in" src="https://thumb.tildacdn.com/tild3565-3966-4631-b638-383162616432/-/format/webp/photo_2023-02-27_143.jpeg" alt="offise-img">
+      <h4 class="pb-2 pt-3  text-[18px] font-semibold text-black font-sans">Дубаи</h4>
+      <p class="py-2 text-[15px] text-black font-light font-sans">Arenco Tower, Dubai Internet City</p>
+      </div>
+      <div class="mb-6">
+      <img class="w-[570px] h-[427.5px] cursor-zoom-in" src="https://thumb.tildacdn.com/tild3737-6632-4532-b061-373165376537/-/format/webp/xkbYLdaYD.jpeg" alt="offise-img">
+      <h4 class="pb-2 pt-3  text-[18px] font-semibold text-black font-sans">Москва</h4>
+      <p class="py-2 text-[15px] text-black font-light font-sans">БЦ White Stone, метро Белорусская</p>
+      </div>
+      <div class="mb-6">
+      <img class="w-[570px] h-[427.5px] cursor-zoom-in" src="../public/offise-img.jpg" alt="offise-img">
+      <h4 class="pb-2 pt-3  text-[18px] font-semibold text-black font-sans">Санкт-Петербург</h4>
+      <p class="py-2 text-[15px] text-black font-light font-sans">Бизнес Центр Ponomarev, метро «Московская»</p>
+      </div>
+      <div class="mb-6">
+      <img class="w-[570px] h-[427.5px] cursor-zoom-in" src="https://thumb.tildacdn.com/tild6161-3838-4237-a537-633437343239/-/format/webp/IMAGE_2023-07-01_200.jpg" alt="offise-img">
+      <h4 class="pb-2 pt-3 text-[18px] font-semibold text-black font-sans">Душанбе</h4>
+      <p class="py-2 text-[15px] text-black font-light font-sans">ул Рудаки 57, Душанбе, Республика Таджикистан</p>
+      </div>
+      </div>
+      </div>
+      </section>
+      <section class="bg-[#F0EEEE] py-[35px] ">
+          <div class="container1">
+            <div class="py-10 flex justify-between">
+              <img class="w-[480px] h-[453px]" src="../public/section-img.png" alt="" />
+              <div>
+                <h3 class="text-[28px] font-normal text-[#403f3f] font-sans leading-10 mb-3">Получите консультацию нашего специалиста</h3>
+                <p class="text-black font-sans font-normal leading-6 mb-8">Укажите свои данные и специалист свяжется с вами в первую свободную минуту</p>
+                <form class="flex flex-col">
+                <input type="text" placeholder="Введите ваше имя" class="placeholder:opacity-40 placeholder:text-[#757575] text-[#757575] bg-[#E0E0E0] text-[14px] font-normal h-[50px] w-[399px] mb-[20px] px-[20px] outline-none border-solid border-[1px] border-[#403F3F]">
+                <input type="text" placeholder="Введите ваш телефон" class=" placeholder:opacity-40 placeholder:text-[#757575] text-[#757575] bg-[#E0E0E0] text-[14px] font-normal h-[50px] w-[399px] mb-[20px] px-[20px] outline-none border-solid border-[1px] border-[#403F3F]">
+                <button class="w-[220px] py-[15px] h-[50px] font-normal text-[#403f3f] text-[14px] cursor-pointer bg-[#cca969] rounded-md outline-none font-sans">Отправить</button>
+                </form>
+                <p class="text-[#d1d0d0] w-[350px] mt-[20px] text-[11px] font-sans ">
+                  Нажимая на кнопку, вы даёте согласие на обработку персональных данных и соглашаетесь с политикой конфиденциальности
+                  </p>
+              </div>
+            </div>
+          </div>
+        </section>
+    </template>
