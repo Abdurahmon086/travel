4d96bb7fd9f20f2badefac622fb308068b119e26
Abdurahmon086 <1937ali047@gmail.com>
2023-11-30 08:52:11 +0500

    footer created

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <header class="fixed backdrop-blur-sm bg-[#00000063] top-0 left-0 z-50 w-full">
+  <header
+    class="fixed backdrop-blur-sm bg-[#00000063] top-0 left-0 z-50 w-full"
+  >
     <div
       class="container mx-auto h-16 flex items-center justify-between border-b-[1px] border-white"
     >
@@ -27,6 +29,101 @@
   <main>
     <NuxtPage />
   </main>
-  <footer><h4>FOOTER</h4></footer>
-</template>
+  <footer>
+    <div class="bg-[#403f3f]">
+      <div class="container container1 mx-auto px-5">
+        <div class="flex justify-between mb-20 pt-16">
+          <div class="flex gap-3">
+            <a href="#">
+              <img
+                class="w-11 h-14"
+                src="./public/logo_goodwill.svg"
+                alt="footer logo"
+              />
+            </a>
+            <div>
+              <h4 class="text-white text-2xl">Goodwill Realty</h4>
+              <p class="text-[#f9f9f9] text-[10px] font-sans font-normal">
+                Агенство недвижимости Дубай
+              </p>
+            </div>
+          </div>
 
+          <div class="flex gap-x-48">
+            <div class="flex flex-col">
+              <a
+                href="#"
+                class="text-[#B0B0B0] text-lg font-normal font-sans my-3"
+              >
+                <span class="mx-3">T:</span>+971 5 886 95492
+              </a>
+
+              <a
+                href="#"
+                class="text-[#B0B0B0] text-lg font-normal font-sans my-3"
+              >
+                <span class="mx-3">E:</span> dubai@goodwillestate.ae
+              </a>
+              <div class="flex justify-center">
+                <span
+                  class="mx-3 my-3 text-[#B0B0B0] text-lg font-normal font-sans"
+                  >А:</span
+                >
+                <a
+                  href="#"
+                  class="text-[#B0B0B0] text-lg font-normal font-sans my-3"
+                >
+                  Arenco tower 1806, Dubai <br />
+                  Internet City, Dubai, UAE
+                </a>
+              </div>
+            </div>
+
+            <div class="flex flex-col">
+              <a
+                class="text-[#B0B0B0] text-lg font-normal font-sans mb-2 mt-1 hover:text-white"
+                href="#"
+                >О компании
+              </a>
+              <a
+                class="text-[#B0B0B0] text-lg font-normal font-sans my-2 hover:text-white"
+                href="#"
+              >
+                Проекты</a
+              >
+              <a
+                class="text-[#B0B0B0] text-lg font-normal font-sans my-2 hover:text-white"
+                href="#"
+                >Преимущества покупки</a
+              >
+              <a
+                class="text-[#B0B0B0] text-lg font-normal font-sans hover:text-white"
+                href="#"
+                >Контакты</a
+              >
+            </div>
+          </div>
+        </div>
+        <div class="flex justify-between gap-x-10 items-center pb-10">
+          <p class="text-[#b0b0b0] text-base font-normal font-sans">
+            (с) Goodwill Realty 2022
+          </p>
+          <div class="flex justify-between gap-x-44">
+            <a
+              href="#"
+              class="text-base text-[#b0b0b0] font-sans font-normal hover:text-[#f9f9f9]"
+            >
+              Разработано Lagom.Design
+            </a>
+            <a
+              href="#"
+              class="text-base text-[#b0b0b0] font-sans font-normal hover:text-[#f9f9f9]"
+            >
+              Политика конфиденциальности
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
